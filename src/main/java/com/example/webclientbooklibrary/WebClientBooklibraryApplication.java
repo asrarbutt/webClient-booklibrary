3fe4e29b946a04316633cb1fb87e25eb320d22fb
asrarbutt <asrarbutt1986@gmail.com>
2022-07-14 16:37:13 +0200

    initial Commit

---
+++ src/main/java/com/example/webclientbooklibrary/WebClientBooklibraryApplication.java
@@ -0,0 +1,13 @@
+package com.example.webclientbooklibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebClientBooklibraryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebClientBooklibraryApplication.class, args);
+    }
+
+}
